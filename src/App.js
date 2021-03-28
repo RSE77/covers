930cb500d9c5cdaa968a897861a5d7c8e43e817a
RSE77 <rysboards@yahoo.com>
2021-03-27 20:55:11 -0400

    started form with formik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Field } from "formik";
+import { TextField, Button } from "@material-ui/core"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div>
+      <Formik initialValues={{ address: "", city:'' }}
+        onSubmit={( data, {setSubmitting, resetForm}) => {
+          setSubmitting(true);
+          //make async call
+          console.log("submit: ", data);
+          setSubmitting(false)
+          resetForm(true);
+        }}
+      >
+      {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+          <Field
+          name="address"
+          placeholder="ex: 123 Xyz St"
+          type="input"
+          as={TextField}
+          />
+          
+          <Field
+          name="city"
+          placeholder="Choose City"
+          as="select">
+          <option value="charleston">Charleston</option>
+          <option value="west_ashley">West Ashley</option>
+          <option value="james_island">James Island</option>
+          <option value="johns_island">John's Island</option>
+          <option value="north_charleston">North Charleston</option>
+          <option value="summerville">Summerville</option>
+          <option value="goose_creek">Goose Creek</option>
+          <option value="moncks_corner">Moncks Corner</option>
+          <option value="hanahan">Hanahan</option>
+          <option value="mt_pleasant">Mt Pleasant</option>
+
+          </Field>
+
+        
+            <Button disabled={isSubmitting} type="submit">submit</Button>
+              <pre>{JSON.stringify(values, null, 2)}</pre>
+            </form>
+
+        )}
+      </Formik>
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ src/components/DatePicker.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DateView from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { Field, ErrorMessage } from 'formik';
+import TextError from './TextError';
+
+function DatePicker(props) {
+    const { label, name, ...rest } = props
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DatePicker
